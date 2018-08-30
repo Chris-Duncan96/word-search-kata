d853accc3a0385d84475b3757d08e90d8882834e
cmduncan <cmduncan@iastate.edu>
2018-08-29 19:38:47 -0500

    Setup tests

---
+++ src/WordSearcher.spec.js
@@ -0,0 +1,7 @@
+const WordSearcher = require('./WordSearcher');
+
+describe('jest setup', () => {
+    it('should import WordSearcher', () => {
+        expect(WordSearcher).toBeTruthy();
+    })
+});
