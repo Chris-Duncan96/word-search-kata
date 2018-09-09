30fa481d69b4e8f120b88c614fd34cc93045d3c5
cmduncan <cmduncan@iastate.edu>
2018-09-08 21:33:41 -0500

    very simple, ineffecient implementation of horizontal search

--- src/findWordsInGrid.js
+++ src/findWordsInGrid.js
@@ -1,5 +1,32 @@
+function getLetterLocations(word, grid) {
+    let letterLocations = '';
+    const wordAsArray = word.split('');
+    let locationInWord = 0;
+
+    grid.map((row, rowIndex) => {
+        row.forEach((letter, letterIndex) => {
+            if(wordAsArray[locationInWord] === letter) {
+                locationInWord++;
+                letterLocations += '('+rowIndex+','+letterIndex+')';
+                if(locationInWord === wordAsArray.length) {
+                    return letterLocations;
+                } else {
+                    letterLocations += ',';
+                }
+            } else {
+                letterLocations = '';
+                locationInWord = 0;
+            }
+        });
+        return letterLocations;
+    });
+    return letterLocations;
+}
+
 function findWordsInGrid(words, grid) {
-    return ['WORD: (0,0),(0,1),(0,2),(0,3)'];
+    return words.map((word) => {
+        return word + ': ' + getLetterLocations(word, grid);
+    });
 }
 
 module.exports = findWordsInGrid;
--- src/findWordsInGrid.spec.js
+++ src/findWordsInGrid.spec.js
@@ -5,23 +5,42 @@ describe('findWordsInGrid', () => {
         expect(typeof findWordsInGrid).toBe('function');
     });
 
-    it('should return an array of strings when given two arrays of strings as arguments', () => {
+    it('should return an object when given two arrays of strings as arguments', () => {
         const words = [''];
-        const grid = [''];
+        const grid = [['']];
 
         const result = findWordsInGrid(words, grid)
         const expected = ['WORD: (0,0),(0,1),(0,2),(0,3)'];
 
         expect(typeof result).toEqual('object');
-        expect(typeof result[0]).toEqual('string');
     });
 
-    it('should be able to find a horizontal word', () => {
+    it('should be able to find a horizontal word when that word is the only thing in the grid', () => {
         const words = ['WORD'];
-        const grid = ['W','O','R','D'];
+        const grid = [['W','O','R','D']];
 
         const result = findWordsInGrid(words, grid)
         const expected = ['WORD: (0,0),(0,1),(0,2),(0,3)'];
+
+        expect(result).toEqual(expected);
+    });
+
+    it('should be able to find a horizontal word when that word when it is not on the first line', () => {
+        const words = ['WORD'];
+        const grid = [['A','A','A','A'],['W','O','R','D']];
+
+        const result = findWordsInGrid(words, grid)
+        const expected = ['WORD: (1,0),(1,1),(1,2),(1,3)'];
+
+        expect(result).toEqual(expected);
+    });
+
+    it('should be able to find a horizontal word when that word when it is aligned on the right side of the grid', () => {
+        const words = ['WORD'];
+        const grid = [['A','W','O','R','D']];
+
+        const result = findWordsInGrid(words, grid)
+        const expected = ['WORD: (0,1),(0,2),(0,3),(0,4)'];
 
         expect(result).toEqual(expected);
     });
