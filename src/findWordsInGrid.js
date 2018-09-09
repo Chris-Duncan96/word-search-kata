582341289d8c470b80f92532540366d62875c309
cmduncan <cmduncan@iastate.edu>
2018-09-09 15:45:12 -0500

    actual minor refactor to look for a specific letter, laying groundwork for unidirectional search

--- src/findWordsInGrid.js
+++ src/findWordsInGrid.js
@@ -2,11 +2,12 @@ function getLetterLocations(word, grid) {
     let letterLocations = '';
     const wordAsArray = word.split('');
     let locationInWord = 0;
+    let nextLetter = wordAsArray[0];
 
     grid.some((row, rowIndex) => {
         row.some((letter, letterIndex) => {
-            if(wordAsArray[locationInWord] === letter) {
-                locationInWord++;
+            if(nextLetter === letter) {
+                nextLetter = wordAsArray[++locationInWord];
                 letterLocations += '('+rowIndex+','+letterIndex+')';
                 if(locationInWord === wordAsArray.length) {
                     return letterLocations;
